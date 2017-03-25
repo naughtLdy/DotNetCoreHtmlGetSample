651913b7369cfcb7e7648583cb1c3579e9778dc2
naughtLdy <sdax05@gmail.com>
2017-03-25 18:29:44 +0900

    リアルタイム検索で話題のワードを取得する

---
+++ DotNetCoreHtmlGetSample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCoreHtmlGetSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            tweetList().Wait();
+        }
+
+        private static async Task tweetList()
+        {
+            var tweetRanking = new TweetRanking();
+            var tweetList = await tweetRanking.getTweetRanking();
+
+            foreach (var tweet in tweetList)
+            {
+                Console.WriteLine("{0}", tweet);
+            }
+        }
+    }
+}
---
+++ DotNetCoreHtmlGetSample/TweetRanking.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DotNetCoreHtmlGetSample
+{
+    class TweetRanking
+    {
+        private static string url = "http://searchranking.yahoo.co.jp/realtime_buzz/";
+
+        public async Task<List<string>> getTweetRanking()
+        {
+            var doc = new HtmlAgilityPack.HtmlDocument();
+
+            using (var client = new HttpClient())
+            using (var stream = await client.GetStreamAsync(url))
+            {
+                var htmlDoc = new HtmlAgilityPack.HtmlDocument();
+                htmlDoc.Load(stream);
+                var wordList = htmlDoc.DocumentNode.SelectNodes("//ul[@class=\"patf\"]/li/h3/a")
+                    .Where(a => a.InnerText != "")
+                    .Select(a => a.InnerText)
+                    .ToList<string>();
+
+                return wordList;
+            }
+        }
+    }
+}
